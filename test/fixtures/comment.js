7046d44a43416613a425f08ea1729a48052c5b75
shellscape <andrew@shellscape.org>
2018-11-21 17:15:52 -0500

    chore: knocking down errors

--- test/fixtures/comment.js
+++ test/fixtures/comment.js
@@ -11,21 +11,12 @@
 module.exports = {
   snapshot: [
     '/*before*/ 1px /*between*/ 1px /*after*/',
-    // TODO: uncomment when function nodes are done
-    // 'rgba( 0, 55/55, 0/*,.5*/ )',
-    // 'url("/demo/bg.png" /*comment*/ )',
     '/**/',
     '//before\n 1px //between\n 1px //after\n',
-    // 'rgba( 0, 55/55, 0//,.5\n )',
-    // 'url(var(//comment\n))',
     '//\n',
     '//',
     '/*before*/\n//between\n/*after*/',
     '//before\n/*between*/\n//after'
-    // 'url( "/demo/bg.png" /*comment*/ )',
-    // 'url( /*comment*/ /demo/bg.png )',
-    // 'url( /demo/bg.png /*comment*/ )',
-    // 'url(//bar\n http://domain.com/foo/bar //foo\n)',
   ],
   throws: ['/*comment*/ 1px /* unclosed ']
 };

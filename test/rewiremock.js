337f1fe9de0a947262f232cf7f12c7bb948a57da
Stéphane Goetz <onigoetz@onigoetz.ch>
2020-09-25 21:43:23 +0200

    Use rewiremock to de-randomize random ids for snapshots

---
+++ test/rewiremock.js
@@ -0,0 +1,12 @@
+const rewiremock = require('rewiremock/node');
+
+// Configure and enable Rewiremock
+rewiremock.overrideEntryPoint(module);
+rewiremock.enable();
+
+// Override the nanoid/non-secure module to produce a predictable nonrandom ID
+// This is necessary because the Input module of PostCSS uses this to generate
+// Random IDs and this breaks snapshots since these are different at each run.
+rewiremock('nanoid/non-secure').with({ nanoid: () => 1 });
+
+module.exports = rewiremock;
